13063b32239cb1a44c2b4b36a3706b43b11088ec
jeiare01-lab <jeiare01@gmail.com>
2026-06-06 04:16:35 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HRCalendar from './HRCalendar'
+import HRCalendar from "./pgb-hr-calendar"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
